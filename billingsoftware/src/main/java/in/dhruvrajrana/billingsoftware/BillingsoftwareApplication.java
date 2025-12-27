912555b62722ef2b78095e458ed266b9102871ee
Dhruvraj-Rana <dhruvrajm.rana@gmail.com>
2025-12-27 18:11:59 +0530

    Backend refactoring

--- billingsoftware/src/main/java/in/dhruvrajrana/billingsoftware/BillingsoftwareApplication.java
+++ billingsoftware/src/main/java/in/dhruvrajrana/billingsoftware/BillingsoftwareApplication.java
@@ -1,7 +1,10 @@
 package in.bushansirgur.billingsoftware;
-
+import java.util.ArrayList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 @SpringBootApplication
 public class BillingsoftwareApplication {
